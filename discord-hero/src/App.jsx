7bc995c3e7473a9222cd02407635cf04637d7db8
Maria Ramirez <maria.ramirez.laguia@gmail.com>
2024-05-01 00:00:45 +0200

    initiated

---
+++ discord-hero/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Landing from './components/Landing'
+
+function App() {
+
+
+  return (
+
+    <div className="App">
+
+      <Landing />
+      <h1>IMAGINE A PLACE...</h1>
+
+
+    </div>
+
+
+
+  )
+}
+
+export default App
---
+++ discord-hero/src/components/Landing.jsx
@@ -0,0 +1,17 @@
+import './../App.css'
+import discordlogo from './../assets/discord-logo-white.png'
+
+const Landing = () => {
+
+    return (
+        <header className='Landing'>
+            <img src={discordlogo} alt="Discord Logo" />
+
+            <nav>
+
+            </nav>
+        </header>
+    )
+}
+
+export default Landing
